15be236710b43a44118d52ae3fd304228c9f791b
MaxsanAI <maxsansamurai@gmail.com>
2026-06-14 14:15:45 +0200

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  output: "server",
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+
+  server: {
+    host: true
+  },
+
+  vite: {
+    ssr: {
+      noExternal: [
+        "rss-parser"
+      ]
+    }
+  }
+});
